f17a9d042548e988343e1f8160364555365c8877
bauti.eth <bautistabaiocchilora@gmail.com>
2025-10-03 12:45:16 -0300

    Bauti/wrapped obol (#3)
    * refactor: use wrapped obol
    
    * chore: re-move unused
    
    * add: test suite

--- src/PendleObolSY.sol
+++ src/PendleObolSY.sol
@@ -2,44 +2,29 @@
 pragma solidity ^0.8.0;
 
 import "@pendle/core/StandardizedYield/SYBase.sol";
-import "./interfaces/IStObol.sol";
 import "./interfaces/IRstObol.sol";
+import "./interfaces/IWstObol.sol";
 
 /// @title PendleObolSY
 /// @author [DAMM Capital](https://dammcap.finance)
-/// @notice The rebasing variant of the liquid staked OBOL token, i.e. rstOBOL. This contract works
-/// in tandem with the fixed balance staked OBOL token, i.e. stOBOL. While the fixed balance stOBOL
-/// is the canonical liquid representation of staked OBOL, the two use the same underlying
-/// accounting system, which is located in this contract. Unlike stOBOL, which maintains a fixed
-/// balance that becomes worth more underlying OBOL tokens overtime, rstOBOL has a dynamic balance
-/// function that update automatically. As such, 1 rstOBOL is always equivalent to 1 underlying
-/// OBOL.
-/// @notice The fixed balance variant of the liquid staked OBOL token, i.e. stOBOL. This contract
-/// works in tandem with the rebasing staked OBOL token, i.e. rstOBOL. While this contract is the
-/// canonical liquid representation of staked OBOL, the two use the same underlying accounting
-/// system, which is located in the rebasing token contract. This contract is deployed by, and
-/// interacts with, the rstOBOL contract. Unlike rstOBOL, this contract maintains a fixed balance
-/// for stakers, even as rewards are distributed. While the user's balance of stOBOL stays fixed,
-/// the number of underlying OBOL tokens they have a claim to grows over time. As such, 1 stOBOL
-/// will be worth more and more OBOL over time.
 contract PendleObolSY is SYBase {
     using PMath for uint256;
 
     address public immutable obol;
-    address public immutable stObol;
     address public immutable rstObol;
+    address public immutable wstObol;
     uint256 public immutable shareScaleFactor;
 
-    constructor(string memory _name, string memory _symbol, address _obol, address _stObol)
-        SYBase(_name, _symbol, _stObol)
+    constructor(string memory _name, string memory _symbol, address _obol, address _wstObol)
+        SYBase(_name, _symbol, _wstObol)
     {
         obol = _obol;
-        stObol = _stObol;
-        rstObol = IStObol(_stObol).LST();
-
-        _safeApproveInf(obol, stObol);
-
+        wstObol = _wstObol;
+        rstObol = IWstObol(_wstObol).LST();
         shareScaleFactor = IRstObol(rstObol).SHARE_SCALE_FACTOR();
+
+        /// @dev this will allow obol to be wrapped
+        _safeApproveInf(obol, wstObol);
     }
 
     /*///////////////////////////////////////////////////////////////
@@ -49,8 +34,6 @@ contract PendleObolSY is SYBase {
     /**
      * @dev See {SYBase-_deposit}
      *
-     * The underlying yield token is stObol. If the base token deposited is obol, the function wraps
-     * it into stObol.
      *
      * The exchange rate of stObol to shares is 1:1
      */
@@ -60,17 +43,17 @@ contract PendleObolSY is SYBase {
         override
         returns (uint256 amountSharesOut)
     {
-        if (tokenIn == stObol) {
+        if (tokenIn == wstObol) {
             amountSharesOut = amountDeposited;
         } else if (tokenIn == obol) {
-            amountSharesOut = IStObol(stObol).stake(amountDeposited);
+            amountSharesOut = IWstObol(wstObol).wrapUnderlying(amountDeposited);
         }
     }
 
     /**
      * @dev See {SYBase-_redeem}
      *
-     * The shares are redeemed into the same amount of stObol.
+     *
      */
     function _redeem(address receiver, address tokenOut, uint256 amountSharesToRedeem)
         internal
@@ -84,10 +67,10 @@ contract PendleObolSY is SYBase {
 
     /**
      * @notice Calculates and updates the exchange rate of shares to underlying asset token
-     * @dev It is the exchange rate of stObol to Obol
+     * @dev this is the exchange rate of wstObol to stObol
      */
     function exchangeRate() public view virtual override returns (uint256) {
-        return IRstObol(rstObol).stakeForShares(1 ether * shareScaleFactor);
+        return IWstObol(wstObol).previewUnwrapToFixed(1 ether * shareScaleFactor);
     }
 
     function _previewDeposit(address tokenIn, uint256 amountTokenToDeposit)
@@ -97,7 +80,7 @@ contract PendleObolSY is SYBase {
         returns (uint256 amountSharesOut)
     {
         if (tokenIn == obol) {
-            return amountTokenToDeposit.divDown(exchangeRate());
+            return IWstObol(wstObol).previewWrapUnderlying(amountTokenToDeposit);
         } else {
             return amountTokenToDeposit;
         }
@@ -115,20 +98,20 @@ contract PendleObolSY is SYBase {
     function getTokensIn() public view virtual override returns (address[] memory res) {
         res = new address[](2);
         res[0] = obol;
-        res[1] = stObol;
+        res[1] = wstObol;
     }
 
     function getTokensOut() public view virtual override returns (address[] memory res) {
         res = new address[](1);
-        res[0] = stObol;
+        res[0] = wstObol;
     }
 
     function isValidTokenIn(address token) public view virtual override returns (bool) {
-        return token == obol || token == stObol;
+        return token == obol || token == wstObol;
     }
 
     function isValidTokenOut(address token) public view virtual override returns (bool) {
-        return token == stObol;
+        return token == wstObol;
     }
 
     function assetInfo() external view returns (AssetType assetType, address assetAddress, uint8 assetDecimals) {
---
+++ src/interfaces/IWstObol.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+library Staker {
+    type DepositIdentifier is uint256;
+}
+
+interface IWstObol {
+    error ECDSAInvalidSignature();
+    error ECDSAInvalidSignatureLength(uint256 length);
+    error ECDSAInvalidSignatureS(bytes32 s);
+    error ERC20InsufficientAllowance(address spender, uint256 allowance, uint256 needed);
+    error ERC20InsufficientBalance(address sender, uint256 balance, uint256 needed);
+    error ERC20InvalidApprover(address approver);
+    error ERC20InvalidReceiver(address receiver);
+    error ERC20InvalidSender(address sender);
+    error ERC20InvalidSpender(address spender);
+    error ERC2612ExpiredSignature(uint256 deadline);
+    error ERC2612InvalidSigner(address signer, address owner);
+    error InvalidAccountNonce(address account, uint256 currentNonce);
+    error InvalidShortString();
+    error OwnableInvalidOwner(address owner);
+    error OwnableUnauthorizedAccount(address account);
+    error StringTooLong(string str);
+    error WrappedGovLst__InvalidAmount();
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event DelegateeSet(address indexed oldDelegatee, address indexed newDelegatee);
+    event EIP712DomainChanged();
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event UnwrapFixed(address indexed holder, uint256 lstAmount, uint256 wrappedAmount);
+    event UnwrapRebasing(address indexed holder, uint256 lstAmount, uint256 wrappedAmount);
+    event WrappedFixed(address indexed holder, uint256 fixedAmount, uint256 wrappedAmount);
+    event WrappedRebasing(address indexed holder, uint256 rebasingAmount, uint256 wrappedAmount);
+    event WrappedUnderlying(address indexed holder, uint256 underlyingAmount, uint256 wrappedAmount);
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+    function FIXED_LST() external view returns (address);
+    function LST() external view returns (address);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 value) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+    function decimals() external view returns (uint8);
+    function delegatee() external view returns (address);
+    function depositId() external view returns (Staker.DepositIdentifier);
+    function eip712Domain()
+        external
+        view
+        returns (
+            bytes1 fields,
+            string memory name,
+            string memory version,
+            uint256 chainId,
+            address verifyingContract,
+            bytes32 salt,
+            uint256[] memory extensions
+        );
+    function name() external view returns (string memory);
+    function nonces(address owner) external view returns (uint256);
+    function owner() external view returns (address);
+    function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
+        external;
+    function previewUnwrapToFixed(uint256 _wrappedAmount) external view returns (uint256);
+    function previewUnwrapToRebase(uint256 _wrappedAmount) external view returns (uint256);
+    function previewWrapRebasing(uint256 _stakeTokensToWrap) external view returns (uint256);
+    function previewWrapUnderlying(uint256 _stakeTokensToWrap) external view returns (uint256);
+    function renounceOwnership() external;
+    function setDelegatee(address _newDelegatee) external;
+    function symbol() external view returns (string memory);
+    function totalSupply() external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function transferOwnership(address newOwner) external;
+    function unwrapToFixed(uint256 _wrappedAmount) external returns (uint256 _fixedTokensUnwrapped);
+
+    function unwrapToRebase(uint256 _wrappedAmount) external returns (uint256);
+    function wrapFixed(uint256 _fixedTokensToWrap) external returns (uint256);
+    function wrapRebasing(uint256 _lstAmountToWrap) external returns (uint256 _wrappedAmount);
+    function wrapUnderlying(uint256 _stakeTokensToWrap) external returns (uint256);
+}
